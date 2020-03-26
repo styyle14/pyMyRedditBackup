17841816cd49abe344c75f1a63a38ec68cb5cb26
styyle14 <styyle14@gmail.com>
2020-03-25 22:53:25 -0500

    initial ability to log in ti Reddit with PRAW

--- pymyredditbackup/pymyredditbackup.py
+++ pymyredditbackup/pymyredditbackup.py
@@ -3,15 +3,27 @@
 
 import getpass
 import logging
+import sys
 
+import praw
 
-# Use root python logger to set loglevel to INFO
-logging.basicConfig(level=logging.INFO)
+import prawcore
 
 
 def main() -> None:
     """Do the main thing."""
+    # Use root python logger to set loglevel to INFO
+    logging.basicConfig(level=logging.INFO)
     username = input("Username: ")
     password = getpass.getpass()
-    logging.info("Username: %s", username)
-    logging.info("Password: %s", password)
+    try:
+        reddit = praw.Reddit(username=username, password=password, user_agent=f"pyMyRedditBackup")
+        logging.info("Status: %s", reddit.user.me())
+    except prawcore.exceptions.ResponseException:
+        logging.critical("Unauthorized client error: Wrong client_id or client_secret in praw.ini")
+        logging.debug("Traceback: ", exc_info=True)
+        sys.exit(1)
+    except prawcore.exceptions.OAuthException:
+        logging.critical("Login authentication error: Wrong username or password")
+        logging.debug("Traceback: ", exc_info=True)
+        sys.exit(2)
